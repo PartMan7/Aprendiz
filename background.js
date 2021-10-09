bbe2503c2b0dd4b7e072eedfebfed3d3532723c5
PartMan <mane.parth@gmail.com>
2021-10-09 20:29:31 +0530

    More languages!

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 const frequency = 0.2;
+const lang = 'french';
 
 chrome.runtime.onInstalled.addListener(() => {
 	chrome.storage.sync.set({ frequency });
 	console.log(`Default frequency set to: ${frequency}`);
-	chrome.storage.sync.set({ lang: 'spanish' });
-	console.log(`Default language set to: Spanish`);
+	chrome.storage.sync.set({ lang });
+	console.log(`Default language set to: ${lang}`);
 });
